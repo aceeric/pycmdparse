00e199613f8f01142d48ed36699489ff3cd95621
Eric Ace <ericace@protonmail.com>
2019-03-11 20:35:06 -0400

    clone_me.py cleanup

--- example/clone_me.py
+++ example/clone_me.py
@@ -45,10 +45,13 @@ class MyCmdLine(CmdLine):
         - short   : z
           long    : tbd-2
           help: >
-            This shows the minimum requirement to define an option. The injected
-            name is derived from the long key if available, else the short key.
-            The option is defined as an option taking one parameter, in this case
-            '--tbd xyz', or -z xyz'
+            This shows the minimum requirement to define a parseable option. The injected
+            name is derived from the long key if available, else the short key. The key
+            is converted to a Python identifier by replacing dashes with underscores. If a
+            valid identifier, it is used, otherwise an error is thrown. Since the option
+            type ('opt') is omitted, pycmdparse defines it as a param option taking exactly
+            one parameter. As defined above, the command line arg would be: '--tbd-2 xyz',
+            or -z xyz'. And the injected field name would be 'tbd_2'.
 
     details: >
       TBD
@@ -73,14 +76,23 @@ class MyCmdLine(CmdLine):
             if some_error_condition:
                 return OptAcceptResultEnum.ERROR, "TODO error message"
         elif isinstance(to_validate, AbstractOpt):
-            if to_validate.opt_name == "tbd1":
+            if to_validate.opt_name == "your_field":
                 if some_error_condition:
                     return OptAcceptResultEnum.ERROR, "TODO error message"
         return None,
 
-    tbd1 = None
-    tbd2 = None
+    # these fields will be injected if not defined. If defined, their values
+    # will be set by the parser
 
+    tbd1 = None   # name defined explicitly in the yaml
+    tbd_2 = None  # no name, so derived from the long key by pycmdparse
+
+
+# To run this from the command line, 'pipenv shell' from the main directory
+# Then: 'python example/clone_me.py --tbd-2 Y --tbd-1 X'
+# Prints:
+# tbd1 = X
+# tbd_2 = Y
 
 if __name__ == "__main__":
     parse_result = MyCmdLine.parse(sys.argv)
@@ -88,5 +100,5 @@ if __name__ == "__main__":
         MyCmdLine.display_info(parse_result)
         exit(1)
     print("tbd1 = {}".format(MyCmdLine.tbd1))
-    print("tbd2 = {}".format(MyCmdLine.tbd2))
+    print("tbd_2 = {}".format(MyCmdLine.tbd_2))
     exit(0)
